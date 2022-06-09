48ec5453be124c9307b617d1133fae3e1ed143c5
Vicentetw <perrottavicente@gmail.com>
2022-06-09 14:40:12 -0300

    instalación algular, bootstrap, bootswach, theme Âen angular.json, y comp, module de header con menú

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { HeaderModule } from './shared/components/header/header.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    HeaderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
