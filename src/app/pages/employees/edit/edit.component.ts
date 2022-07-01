3b18254d5da47c431e15d235101793e34eb70f63
Vicentetw <perrottavicente@gmail.com>
2022-07-01 10:03:49 -0300

    Formulario edit

--- src/app/pages/employees/edit/edit.component.ts
+++ src/app/pages/employees/edit/edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,15 +9,27 @@ import { Router } from '@angular/router';
 })
 export class EditComponent implements OnInit {
 value;
-
-
-  constructor(private router: Router) {
+employeeForm!: FormGroup;
+// para email corto private isEmail = /\S+@\S+\.\S+/;
+private isEmail = /^(([^<>()\[\]\\.,;:\s@”]+(\.[^<>()\[\]\\.,;:\s@”]+)*)|(“.+”))@((\[[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}])|(([a-zA-Z\-0–9]+\.)+[a-zA-Z]{2,}))$/;
+  constructor(private router: Router, private fb: FormBuilder) {
     const navigation = this.router.getCurrentNavigation();
     this.value = navigation?.extras?.state;
-    console.log('Value',this.value);
+    
    }
 
   ngOnInit(): void {
+    this.initForm();
   }
-
+onSave(): void{
+  console.log('saved', this.employeeForm.value)
+}
+private initForm(): void {
+  this.employeeForm = this.fb.group({
+  name:['', [Validators.required]],
+  lastName:['', [Validators.required]],
+  email:['', [Validators.required, Validators.pattern(this.isEmail)]],
+  startDate:['', [Validators.required]]
+})
+}
 }
