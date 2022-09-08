0d2e7ff22a7212ee2b880d33bf9a554db6953518
Vicentetw <perrottavicente@gmail.com>
2022-09-08 12:42:54 -0300

    routing app.component login sing-in and auth.service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,14 +6,16 @@ import { LoginComponent } from './pages/login/login/login.component';
 import { ForgotPasswordComponent } from './pages/login/forgot-password/forgot-password.component';
 import { VerifyEmailComponent} from './pages/login/verify-email/verify-email.component';
 
-const routes: Routes = [{ path: 'list', loadChildren: () => import('./pages/employees/list/list.module').then(m => m.ListModule) },
- { path: 'new', loadChildren: () => import('./pages/employees/new/new.module').then(m => m.NewModule) }, 
+const routes: Routes = [
+  
+  { path: 'list', loadChildren: () => import('./pages/employees/list/list.module').then(m => m.ListModule) },
+{ path: 'new', loadChildren: () => import('./pages/employees/new/new.module').then(m => m.NewModule) }, 
  { path: 'details', loadChildren: () => import('./pages/employees/details/details.module').then(m => m.DetailsModule) }, 
  { path: 'edit', loadChildren: () => import('./pages/employees/edit/edit.module').then(m => m.EditModule) }, 
  { path: 'dashboard', loadChildren: () => import('./pages/dashboard/dashboard/dashboard.module').then(m => m.DashboardModule) },
  { path: '', loadChildren: () => import('./pages/dashboard/dashboard/dashboard.module').then(m => m.DashboardModule) },
  //{ path: '**', redirectTo:'',pathMatch:'full' }]
- { path: '**', redirectTo:'/sign-in',pathMatch:'full' },
+ { path: '**', redirectTo:'/login',pathMatch:'full' },
  { path: 'sign-in', component: SignInComponent },
  { path: 'register-user', component: SignUpComponent },
  { path: 'login', component: LoginComponent },
